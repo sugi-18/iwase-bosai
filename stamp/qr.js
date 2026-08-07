60b862ef885448b67fa8d275141acf82261788b5
sugi-18 <tfs.runbird@gmail.com>
2026-08-07 15:22:05 +0900

    Create qr.js

---
+++ stamp/qr.js
@@ -0,0 +1,22 @@
+function goStamp(){
+
+localStorage.setItem(
+
+"qrTraining",
+
+JSON.stringify({
+
+date:"2026-08-07",
+
+name:"夏季防災訓練",
+
+detail:"消火器訓練・AED訓練"
+
+})
+
+);
+
+
+location.href="index.html";
+
+}
